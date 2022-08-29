68feddd738c2f4b432f4d03d3394f1617e3eea39
ammar-tanwar <ammartanwar123@gmail.com>
2022-08-29 19:28:27 +0530

    thank you webinar

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -10,7 +10,7 @@ import addDays from "date-fns/addDays";
 import subDays from "date-fns/subDays";
 import getDay from "date-fns/getDay";
 
-const Form = ({ popup, setTrigger, downloadBrochure, radio }) => {
+const Form = ({ popup, setTrigger, downloadBrochure, radio, event }) => {
   const router = useRouter();
   let today = new Date();
   let time =
@@ -136,6 +136,10 @@ const Form = ({ popup, setTrigger, downloadBrochure, radio }) => {
     ) {
       router.push("/Thank-you");
       return;
+    }
+
+    if (event) {
+      router.push("/event/Thank-You-event");
     }
 
     if (
