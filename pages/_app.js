e4c863a48ab4949b77412eae9069356b227c5df1
ammar-tanwar <ammartanwar123@gmail.com>
2022-09-28 21:47:33 +0530

    app.js code remove

--- pages/_app.js
+++ pages/_app.js
@@ -8,34 +8,6 @@ import Script from "next/script"
 function MyApp({ Component, pageProps }) {
   return (
     <>
-<Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=UA-215989751-1`}
-      />
-
-<Script
-      strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=GTM-NN8XWH8`}
-      />
-      <Script strategy="lazyOnload">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'GTM-NN8XWH8', {
-              page_path: window.location.pathname,
-            },);
-                `}
-      </Script>
-      <Script strategy="lazyOnload">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', 'UA-215989751-1');
-                `}
-      </Script>
       <Component {...pageProps} />
        
       <BottomBar />
