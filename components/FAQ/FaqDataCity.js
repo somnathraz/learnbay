83e5ca2b0ed6e5a570a6b0211198a58f9b7185ec
ammar-tanwar <ammartanwar123@gmail.com>
2022-11-25 13:21:01 +0530

    new data science india page

--- components/FAQ/FaqDataCity.js
+++ components/FAQ/FaqDataCity.js
@@ -11832,3 +11832,268 @@ export const FullStackSupportFaqDataM = [
     open: false,
   },
 ];
+
+
+
+//  Course Best data science course training in India
+
+export const AdvanceFaqDataI = [
+  {
+    id: 0,
+    ques: "What are Data Science Training courses available at Learnbay in India?",
+    linkText1: "Foundational Data Science Training Course, ",
+    linkText2: "Advance Data Science Certification Course, ",
+    linkText3: "Data Science Certification Course for Managers and Leaders, ",
+    linkText4: "Data Science Training with Placement Assistance",
+    link1: "/data-science-certification-courses",
+    link2: "/advance-data-science-certification-courses",
+    link3: "/data-science-ai-for-managers",
+    link4: "/data-science-ai-masters-program",
+    link: true,
+    open: true,
+  },
+  {
+    id: 1,
+    ques: "What are the prerequisites for these Data Science training Courses in India?",
+    ans: "There are no prerequisites for this Data Science Foundation course as all modules are beginner-friendly and are taught from scratch. We provide special classes and assistance for Non-IT/Non-Programmers. However, basic knowledge of applied Mathematics/Statistics is a must.",
+    open: false,
+  },
+  {
+    id: 2,
+    ques: "What if I miss a few classes due to an emergency?",
+    ans: `In case you miss a few classes, you will be provided with backup classes in other batches.
+    But if you could not attend more classes, you can opt for batch change and join the next batch. `,
+    open: false,
+  },
+  {
+    id: 3,
+    ques: "What is a Flexi Subscription in these advanced data science courses in India?",
+    ans: "Those who enroll for Data Science live classroom training are eligible for a Flexi Pass. With this option, we will share access to all the ongoing batch details for a period of 1 -3 years, so that you can attend live sessions from any batch and learn at your own pace. This option is best for people working in shifts or on weekends. Flexi Subscription Time-line: Foundational Data Science and AI Training course- 1 Year, Advance Data Science and AI Certification course- 3 Years, Data Science and AI Certification Course for Managers and Leaders- 3 Years, Data Science and AI Training with Placement Assistance  - 3-years.",
+    open: false,
+  },
+  {
+    id: 4,
+    ques: "What’s the duration of these advanced data science courses in India?",
+    ans: "The duration of these courses is about 6-15 months, which includes live lectures, hands-on practical training on live projects, and interview preparations. Classes will be conducted on weekdays and weekend batches. Monday to Friday - 2 hours/day and Saturday & Sunday - 3.5 hours/day, Foundational Data Science and AI Training course (220+ hours) :- Weekday batch - 6 months, Weekend batch - 8 months, Advance Data Science and AI Certification course (270+ hours) :- Weekday batch - 7 months, Weekend batch - 9  months, Data Science and AI Certification Course for Managers and Leaders (340+ hours) :- Weekday batch - 11 months, Weekend batch - 13  months, Data Science and AI Training with Placement Assistance (380+ hours) :- Weekday batch - 13 months, Weekend batch - 15  months",
+    open: false,
+  },
+  {
+    id: 5,
+    ques: "What is the mode of Data Science Course In India at Learnbay?",
+    ans: "We provide both classroom and online data science training modes. Based on your requirement, you can choose your preferred mode.",
+    open: false,
+  },
+  {
+    id: 6,
+    ques: "How can I opt for a Hybrid mode of learning in IBM data science training courses in India, At Learnbay? ",
+    ans: "Simply, you can choose an online mode of study for the theoretical classes and a classroom mode for all the capstone projects and interview preparation sessions. ",
+    open: false,
+  },
+];
+export const AdvanceDomainFaqDataI = [
+  {
+    id: 0,
+    ques: "What is domain specialization in the professional data science certification courses provided by Learnbay in India?",
+    ans: "We offer industry-specific full-stack data science modules, via which you learn the respective industry’s best practices. But this added benefit is available in all the three mentioned courses, except the foundational data science course. ",
+    open: true,
+  },
+  {
+    id: 1,
+    ques: "What are the available elective domain options in the different data science training courses of Learnbay in India?",
+    ans: " Advanced Data Structure & algo; Automotive, IoT & Embedded; Cloud & DevOps; Sales, Marketing, & HR; Retail, E-Commerce, & Supply Chain; Healthcare, Pharma, & Clinical Research; BFSI; Manufacturing, Mechanical, & Telecom; Media, Hospitality, & Transportation; Oil, Gas, & Energy",
+    open: false,
+  },
+  {
+    id: 2,
+    ques: "Which data science specialization domain elective is best suited for me?",
+    ans: `You can select your domain based on your prior work experience, academics, and career goals. 
+    Book a slot with our industry expert for free profile review and career guidance.`,
+    linkText1: " +917795687988 ",
+    link1: "tel:+917795687988",
+    link: true,
+    open: false,
+  },
+  {
+    id: 3,
+    ques: "Why is domain specialization important in professional data science certification?",
+    ans: "After enrolling in the advanced data science courses, one must opt for domain electives. It is essential to have business/industry knowledge that is useful in feature engineering and modeling. Prior knowledge of the industry and domain aids in the development of more precise and accurate predictive models based on the dataset's attributes. Furthermore, the model will be more observable in a real-world situation.",
+    open: false,
+  },
+  {
+    id: 4,
+    ques: "Can I select multiple domain electives and do I need to pay extra charges in any of the eligible data science courses?",
+    ans: `You can select a pre-set maximum number (based on course) of domain electives (both tech and non-tech) based on your interest and career goals. 
+    Advance Data Science and AI Certification course- Maximum 2 domains. 
+    Data Science and AI Certification Course for Managers and Leaders- All domains 
+    Data Science and AI Training with Placement Assistance- All domains
+     
+    No, you don’t need to pay any extra charges as domain electives and capstone projects are part of our curriculum.`,
+    open: false,
+  },
+  {
+    id: 5,
+    ques: "What if I don't have any prior experience in any domain?",
+    ans: " If you don't have any prior work experience, you can opt for any electives to gain domain expertise and work on real-time industrial projects.",
+    open: false,
+  },
+  {
+    id: 6,
+    ques: "Can I change my domain electives later?",
+    ans: "Yes, you can change your electives or repeat the training anytime but within the course accessibility duration.",
+    open: false,
+  },
+];
+export const AdvancePaymentFaqDataI = [
+  {
+    id: 0,
+    ques: "What is the total fee for Data Science Certifications courses in India?",
+    ans: `The total course fee for the 
+    Foundational Data Science and AI Training course is 76,700/- inclusive of GST.
+    Advance Data Science and AI Certification course is 93,220/- inclusive of GST.
+    Data Science and AI Certification Course for Managers and Leaders is 1,12,100/- inclusive of GST.
+    Data Science and AI Training with Placement Assistance is 1,47,500/- inclusive of GST.`,
+    open: true,
+  },
+  {
+    id: 1,
+    ques: "Can I pay in installments for these Data Science Certification courses in India?",
+    ans: "Yes, you can pay the installments using a credit card with a no-cost EMI option for up to 9 months. You can also choose an interest-free loan by submitting an Aadhar card, Pan Card, and 3-month salary slip.",
+    //Check This
+    open: false,
+  },
+  {
+    id: 2,
+    ques: " Is there any scholarship/discount available for these Data Science training courses available in India? What is Group Discount?",
+    ans: `Learnbay offers a 15 - 20% scholarship for early birds occasionally.
+    Please check our websites or contact our sales team to know more about the applicable discounts.
+    
+    
+    Group Discount :
+    Group Discount is applicable only if you’re joining with your friends. 
+    5% extra discount: Group of 2
+    8% extra discount: Group of 3
+    Max 10% discount: Groupe of 4+`,
+    open: false,
+  },
+  {
+    id: 3,
+    ques: "What is the refund policy for the Data Science Certification courses at Learnbay?",
+    ans: `Candidates are advised to read  `,
+    linkText1: "Learnbay’s Refund/Cancellation Policy",
+    link1: "/refund-cancellation-policy",
+    ansAfter: " before enrolling in the course.",
+    link: true,
+    open: false,
+  },
+];
+export const AdvanceCapstoneFaqDataI = [
+  {
+    id: 0,
+    ques: "How many capstone projects will be there? ",
+    ans: `Depending on the course you choose the number of capstone projects will vary.  
+    Foundational Data Science and AI Training Course - 1 Capstone.
+    Advance Data Science and AI Certification Course -2 Capstones.
+    Data Science and AI Certification Course for Managers and Leaders- 2 Capstones.
+    Data Science and AI Training with Placement Assistance- 3 Capstones.
+    How many real-time projects/case studies will be covered in these Data Science certification courses in India?
+    Depending on the course you choose the number of live projects will vary.  
+    Foundational Data Science and AI Training course- 8 projects.
+    Advance Data Science and AI Certification course-12 projects.
+    Data Science and AI Certification Course for Managers and Leaders- 15 projects.
+    Data Science and AI Training with Placement Assistance- 15 projects.`,
+    linkText1: " Click here to download the list of the projects.",
+    link1:
+      "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/learnbay/Data+Science+and+AI+Projects.pdf",
+    link: true,
+    open: true,
+  },
+  {
+    id: 1,
+    ques: "How will the projects be executed in India?",
+    ans: "At Learnbay, you will be required to work in a group where your project group will be assigned a specific project mentor with complete assistance and guidance.",
+    open: false,
+  },
+];
+export const AdvanceCertificationFaqDataI = [
+  {
+    id: 0,
+    ques: "Is there any accredited data science certification after course completion?",
+    ans: `Yes, you will be awarded IBM accredited Data Scientist Certificate on successful course completion.  But Based on the course you choose you can get Multiple certification options too.
+    Foundational Data Science and AI Training course- 1 Course Completion Certificate.
+    Advance Data Science and AI Certification course-1 Course Completion Certificate + 1-project experience certificate + 4 microdata science skills certificates = 6 Certificates.
+    Data Science and AI Certification Course for Managers and Leaders- 1 Course Completion Certificate + 1-project experience certificate + 6 microdata science skills certificates= 8 Certificates.
+    Data Science and AI Training with Placement Assistance- 1 Course Completion Certificate
+    *All the certificates accredited by IBM. For more details, please refer to the respective course brochures.`,
+    open: true,
+  },
+];
+export const AdvancejobFaqDataI = [
+  {
+    id: 0,
+    ques: "What's the eligibility for a job assistance Program in Learnbay Data science training courses in India?",
+
+    ans: `You should have completed the course with :
+    A minimum score of 70% in assessments
+   Should have completed the assignments and case studies
+   Worked and successfully completed the minimum recommended number of real-time projects.`,
+    open: true,
+  },
+  {
+    id: 1,
+    ques: "How long will I get support for job assistance at Learnbay in India?",
+    ans: `At Learnbay, you’ll be provided with job assistance after the completion of the course for a period of 1 year.`,
+    open: false,
+  },
+  {
+    id: 2,
+    ques: "What is a job-readiness program?",
+    ans: `The Job readiness program helps you prepare for interviews which include:
+    Resume preparation sessions, 
+    Preparing projects for a strong portfolio, 
+    Interview guidance and prep sessions, 
+    1:1 Mock interviews based on targeted companies and roles.`,
+    open: false,
+  },
+  {
+    id: 3,
+    ques: "Will I get job referrals at Learnbay?",
+    ans: `Yes, we offer dedicated placement assistance by referring to your profile through our partnered consultancies and companies. `,
+    open: false,
+  },
+  {
+    id: 4,
+    ques: "What is an interview guarantee feature?",
+    ans: `As a special feature, we provide interview assurance. That means once you complete the course by fulfilling all the scoring and attendance-related criteria, we’ll provide assured interview calls and job referrals. You will get this facility until you get placed.`,
+    open: false,
+  },
+];
+export const AdvanceMentorshipFaqDataI = [
+  {
+    id: 0,
+    ques: "Until when do the students get the facility of mentorship?",
+
+    ans: "Our mentors will provide data science training to the students until the completion of the course. ",
+    open: true,
+  },
+  {
+    id: 1,
+    ques: "Can a Learnbay student in India choose to study from their desired mentor?",
+    ans: "Every batch has its respective mentor, but if a student is not satisfied with its mentor’s teaching method, they can raise the issue to management and resolve the issues by replacing them with other mentors.",
+    open: false,
+  },
+];
+export const AdvanceSupportFaqDataI = [
+  {
+    id: 0,
+    ques: "When is the job assistance provided at Learnbay and how long will I get it?",
+
+    ans: "Job assistance is provided after the completion of the course. It includes build-up sessions, mock interviews, and referrals from top MNC’s/startups. This assistance is provided unless one gets placed in a company. ",
+    open: true,
+  },
+  {
+    id: 1,
+    ques: "How long will I get Job referrals?",
+    ans: "You’ll be provided with job assistance after the completion of the course for a period of 1 year.",
+    open: false,
+  },
+];
