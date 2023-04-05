7355483b16d7d2d100b7f714bc9085cfcb8f2989
Mohd-w123 <codewithwazid@gmail.com>
2023-04-05 12:36:59 +0530

    city page updated

--- components/CityComponent/CitiesRight/CitiesRightdomain.js
+++ components/CityComponent/CitiesRight/CitiesRightdomain.js
@@ -47,6 +47,7 @@ const CitiesRight = ({
      Asrc1, AlumniH1, Alumnip1, Asrc2, AlumniH2, Alumnip2, Asrc3, AlumniH3, Alumnip3, Asrc4, AlumniH4, Alumnip4, 
      alt1, alt2, alt3, alt4, alt5, alt6, alt7, alt8, alt9,
      cityTextDAbangalore,
+     cityTextDAhyderabad, 
   }) => {
   return (
     <section className={styles.CitiesRightHead}>
@@ -54,6 +55,210 @@ const CitiesRight = ({
             <div className={styles.CitiesRightSpan}>
                 <span>About The Program</span>
             </div>
+            {cityTextDAhyderabad ? (
+          <div className={styles.CitiesRightInner}>
+            <p>Learnbay's <b>data analytics course in Hyderabad</b> might be exactly what you need if you wish 
+                to pursue a career as a data analyst. <b>As the best data analytics training institute in Hyderabad</b>, 
+                Learnbay offers a wide range of IBM certification courses. Our <b>Data Analytics training</b> prepares
+                 you for complex analytical problems in different industries.</p>
+
+             <p>
+             Learnbay aims to provide specialized <b>online data analytics training</b> that gives learners the knowledge and 
+             skills necessary to succeed in their careers as data analysts. The institute handles everything in the 
+             data analytics course, from hands-on training through live data analytics projects to interview preparation 
+             for top-tier MNCs. Renowned industry professionals lead our one-on-one doubt-clearing sessions. 
+             Learnbay delivers immersive <b>data analytics training</b> that sets it apart as a one-stop resource for anyone 
+             looking to become a professional job-ready data analyst.
+             </p>
+
+             <p>
+             One of the key features that Learnbay offers is its flexibility. Our <b>online Data Analytics training</b>{" "}
+             is designed to match the industry's ever-changing needs. We also offer job placement support suited 
+             for both experienced working professionals and early career professionals in any sector. The course's 
+             hands-on learning process incorporates interactive sessions with renowned experts.
+              The industry projects provide in-depth coverage of data analysis, data visualization, and regression techniques.
+             </p>
+
+             <p>By enrolling in Learnbay's <b>data analytics course in Hyderabad</b>, you'll thoroughly understand the fundamental ideas 
+                and real-world uses of data analytics. Throughout the course, you will have access to a wealth of resources and
+                 receive individualized support from subject matter experts in the field, making it simpler for you to accomplish 
+                 your professional objectives. Whether you want to work for a multinational corporation or start your own data
+                  analytics venture, this course will prepare you for success. So why wait? Sign up for our <b>data analytics course</b>{" "} 
+                  today and embark on a journey toward a rewarding career in data analytics.</p>
+
+            <p>Our <b>data analytics course in Hyderabad</b> also offers learners the opportunity to gain practical experience by working on
+                 real-world data analytics projects. With the help of these projects, learners learn to apply their knowledge and 
+                 skills to solve complex problems businesses and organizations face. It gives them a competitive edge in the job market. </p>
+
+            <p>Furthermore, Learnbay has a strong industry connection. It ensures that learners are exposed to the latest
+                 trends and practices in data analytics. <b>Data analytics training</b> makes them more valuable to potential
+                  employers. With its flexible online format, job placement support, and practical training, our <b>data 
+                  analytics course in Hyderabad</b> is the ideal choice for anyone looking to build a successful career in data analytics.</p>
+            <p>
+            We cover the <b>data analytics course</b> using various modules, some of which are mentioned below: 
+            </p>
+
+            <ul>
+              <li>Cohort Orientation</li>
+              <li>Special Programming Classes</li>
+              <li>Basic Python Programming</li>
+              <li>Special Python Programming </li>
+              <li>Statistics</li>
+              <li>Machine Learning </li>
+              <li>SQL</li>
+              <li>Tableau</li>
+              <li>Deep Learning</li>
+              <li>Essentials</li>
+            </ul>
+
+        <p>The course duration of the online Data Analytics training in Hyderabad is 180+ hours.
+             Our trained professionals will tutor you for almost 9 months. </p>
+             <p>The structured course consists of 4 terms. </p>
+
+             <b>Module 0: Preparatory Session </b>
+
+             <p>In this module, you will get a brief introduction to data-related tools. 
+                You will also learn about particular real-time projects and capstone projects. 
+                Data has a lot of impact on career opportunities and plays a vital role in the success of a business.
+                 Some other topics that learners will learn about are the significance of 
+                 data in decision-making, the scope of data in research and development, the fundamentals of programming
+                  and statistics, including an introduction to Linear Algebra. </p>
+
+
+            <h2>Term 1: Python Programming</h2>
+            <b>Module 1: Basic and Advance Python</b>
+            <p>In module 1, our experts will teach you the programming basics and environment setup along with python data types and
+                 functions and modules. Learners will also learn briefly about File I/O And Exceptional Handling and Regular Expression.
+                  An introduction to NumPy, Array Creation, Printing Arrays, introduction to Pandas, and indexing data frames are a
+                   few other topics that will be covered in this module. </p>
+
+            <h2>Term 2: Statistics and Machine Learning </h2>
+            <b>Module 1: Statistics </b>
+
+            <p>The next lesson, vital for data science, focuses on teaching fundamental and statistical ideas.
+                 After this program, you will understand probability, inferential and descriptive statistics, 
+                 exploratory data analysis (EDA), and data processing. You will also be able to complete four practice 
+                 assignments relating to statistics to enrich your learning experience further. You can use your 
+                 newly learned information in these practical exercises, promoting experiential learning.</p>
+
+            <h2>Module 2: Machine Learning </h2>
+
+            <p>This module gives you access to the most current machine-learning methods and tools necessary for building
+                 models and reaching wise conclusions. The advanced machine learning techniques covered in this subject include 
+                 support vector machines, principal component analysis (PCA), clustering, K-nearest neighbours (KNN) models,
+                  and regression and classification models. (SVM). </p>
+            <p>Your instructor will walk you through each of these methods, giving you in-depth explanations and examples
+                 from the real world to help you understand. After finishing this subject, you'll be prepared to use 
+                 cutting-edge machine-learning techniques to build solid models and analyze complex data sets. </p>
+
+            <h2>Term 3: Data Analytics Tools </h2>
+
+            <p>
+              <b>Module 1 - SQL</b>
+            </p>
+            <p>
+              <b>Module 2: PowerBI</b>
+            </p>
+            <p>
+              <b>Module 3: Big Data and Spark Analytics</b>
+            </p>
+            <p>
+              <b>Module 4: Time Series</b>
+            </p>
+           
+           <h2>Term 4: Excel </h2>
+           <b>Module 1: Essentials</b>
+
+           <h2>Data Analytics Certification Program in Hyderabad with Placement Assistance</h2>
+
+           <ul>
+            <li>This course gives you 1.5 years of limitless access to your learning materials and live class recordings</li>
+            <li>You can choose weekdays or weekends to take the course</li>
+            <li>Experts from MNCs and MAANG assist in online and offline project sessions</li>
+            <h3>Highlights of Data Analytics Certification Program in Hyderabad</h3>
+            <li>180+ hours of live sessions </li>
+            <li>8+ real-time industrial projects</li>
+            <li>Live Project experience</li>
+            <li>97% average salary hike</li>
+            </ul>
+
+            <h2>Why Should You Choose Learnbay’s Data Analytics Course in Hyderabad? </h2>
+            <p>Learnbay offers the best data analytics online training. Below mentioned are a 
+                few key reasons why you should choose us. </p>
+
+          <ol type='1'>
+            <li><b>100% live interactive training:</b> The course includes live interactive training sessions
+             led by experienced data analysts. This allows students to interact in real-time with peers and instructors.</li>
+
+             <li><b>Flexible scheduling:</b> It is a feature of Learnbay's data analytics course that makes it simpler for students 
+                to juggle their academic obligations with their other commitments.</li>
+
+            <li><b>Sessions guided by experienced data analysts:</b> Learnbay's instructors are skilled data analysts with years of
+                 experience working in the sector, providing students with an insightful understanding of practical data analytics techniques.</li>
+
+            <li><b>One-year flexi membership:</b> It is included with the course, enabling students to go at their own pace and go
+             over the course material whenever necessary.</li>
+
+             <li><b>Choose your favourite batch and mentors:</b> Students can pick their preferred batch and mentors to tailor their learning experience.</li>
+          </ol>
+
+          <h2>Data Analytics Trends in Hyderabad</h2>
+
+          <p>The city of Hyderabad is quickly becoming a center for data analytics, thanks to the establishment of numerous businesses 
+            there. Making judgments based on data using artificial intelligence and machine learning is one of Hyderabad's
+             notable data analytics trends. Businesses use these technologies to analyze massive volumes of data rapidly and correctly, 
+             giving them access to insightful information about consumer behaviour, market trends, and corporate operations. </p>
+
+             <p>The usage of cloud-based analytics solutions is another trend that is becoming more and more popular in Hyderabad.
+                 Businesses can now store and process massive volumes of data on the cloud thanks to the development 
+                 of cloud-based analytics tools. It allows them to scale their operations quickly while paying the least
+                  for infrastructure.</p>
+
+            <p>The use of data analytics in the healthcare industry is another trend that is gaining popularity in Hyderabad.
+                 Healthcare providers are now utilizing data analytics to enhance patient outcomes and lower healthcare costs 
+                 due to the growing adoption of electronic health records and the availability of vast patient data. Data analytics
+                  locates high-risk people, predicts disease outbreaks, and creates personalized treatment plans. In the upcoming years, 
+                  the use of data analytics in the healthcare industry is anticipated to expand quickly, making Hyderabad a desirable 
+                  location for data analytics and healthcare businesses.</p>
+
+            <h2>Best Data Analytics Training Institute in Hyderabad</h2>
+            <p>Learnbay is considered the <b>best data analytics institute in Hyderabad</b>. Learnbay has established a reputation 
+                for turning out highly trained data professionals by emphasizing training that applies to the business.
+                 The institute provides a wide range of data analytics courses, including big data analytics, artificial intelligence, 
+                 machine learning, and data science. </p>
+
+            <p>The programs are meant to give students a thorough understanding of the sector’s various data analytics technologies and methods. 
+                The Learnbay faculty comprises skilled data experts with a proven track record of achievement. Students work on actual 
+                data analytics projects as part of the institute's innovative teaching style, which gives them the hands-on experience
+                 they need to thrive in the field. In general, Learnbay is a great option for anyone in Hyderabad who wants to 
+                 pursue a career in data analytics.</p>
+
+            <h2>Did You Know? </h2>
+
+            <p>Data Analytics is a lucrative career option for people interested in the profession because the <b>average data 
+                analyst salary in Hyderabad</b> is INR 6 lakhs.</p>
+
+            <p>The <b>Data Analytics Course in Hyderabad</b> is gaining momentum with the increasing demand for data analytics in India. 
+                Hyderabad is quickly becoming its center, and local companies realize the value of data-driven decision-making. 
+                It's hardly surprising that Hyderabad is seeing an increase in demand for qualified data analysts, given that 
+                the amount of data produced globally is expected to exceed 463 exabytes by 2025.</p>
+
+            <p>The demand for knowledgeable experts in the industry is increasing as businesses in Hyderabad rely more heavily 
+                on data analytics. Learnbay offers the <b>best data analytics training in Hyderabad</b> to meet the growing demand for
+                 skilled professionals.</p>
+
+            <p>Learnbay's <b>Data Analytics Course in Hyderabad</b> is a great place to start if you're interested in a career in data analytics.
+                 A course like this might give you the information and abilities required to be successful in this profitable career, 
+                given the city's growing demand for data analytics professionals.</p>
+
+           <p> Learnbay also provides offline project sessions for{" "}
+             <a href="https://www.learnbay.co/machine-learning-course-training-in-hyderabad" target="_blank" style={{ color: "#2D9CD7", fontWeight: "600" }}>machine learning</a> and {" "}
+             <a href="https://www.learnbay.co/artificial-intelligence-ai-course-training-hyderabad" target="_blank" style={{ color: "#2D9CD7", fontWeight: "600" }}>artificial intelligence courses in Hyderabad</a>.</p>
+         
+      </div>
+        ) : ("")}
+
+
             {cityTextDAbangalore ? (
           <div className={styles.CitiesRightInner}>
           <p>
@@ -348,6 +553,8 @@ What are you waiting for? Grab your chance and register for the best <b>Data Ana
                 <span>Frequently Asked Questions</span>
             </div>
         </div>
+
+        
     </section>
   )
 }
