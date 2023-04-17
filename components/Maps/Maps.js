097b1803adc8ec6149980f7dd270cc7344cac2d1
Mohd-w123 <codewithwazid@gmail.com>
2023-04-17 17:44:19 +0530

    map removed

--- components/Maps/Maps.js
+++ components/Maps/Maps.js
@@ -4,8 +4,8 @@ const Map = () => {
   return (
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.936546640914!2d77.63858847401396!3d12.911799816188676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae13b203864921%3A0xcd2b3d47edc34b69!2sLearnbay!5e0!3m2!1sen!2sin!4v1681469062178!5m2!1sen!2sin"
-      width="400"
-      height="350"
+    //   width="1440"
+      height="400"
       loading="lazy"
     ></iframe>
   );
