b2cec1e0b50326629d679ff3fd8d8cc712c52eab
ammar-tanwar <ammartanwar123@gmail.com>
2023-06-27 18:56:34 +0530

    dsa new design

--- pages/dsa-system-design.js
+++ pages/dsa-system-design.js
@@ -1,34 +1,35 @@
+import React, { useState } from "react";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import FirstSection from "../components/CoursePage/HeroSection/HeroSection";
-import ProgramInfo from "../components/CoursePage/ProgramInfo/ProgramInfo";
-import BoxShape from "../components/CoursePage/Boxshapedsas/BoxShape";
-import Popup from "../components/Popup/Popup";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import BottomBar from "../components/BottomBar/BottomBar";
 import Form from "../components/Form/Form";
-import Certificate from "../components/CoursePage/CertificateTab/CertificateTabWeb";
-import Project from "../components/CoursePage/Projectfswd/Project";
-import CourseFee from "../components/CoursePage/CourseFee/CourseFee";
-import SyllabusNew from "../components/CoursePage/SyllabusNew/SyllabusNew";
-import DomainFaq from "../components/CoursePage/DomainFaq/DomainFaq";
-import CourseReview from "../components/CoursePage/CourseReviewdsas/CourseReview";
-import { DomainFaqCourseDataDSA } from "../components/CoursePage/DomainFaq/DOmainFaqData";
-import { ProgramFee } from "../components/CoursePage/ProgramFee/ProgramFee";
-import { DsaCourseData } from "../Data/DsaData";
+import Popup from "../components/Popup/Popup";
+import FirstSection from "../components/NewDesign/FirstSection/FirstSection";
+import SecondSection from "../components/NewDesign/SecondSection/SecondSection";
+import ThirdSection from "../components/NewDesign/ThirdSection/ThirdSection";
+import FourthSection from "../components/NewDesign/FourthSection/FourthSection";
+import FifthSection from "../components/NewDesign/FifthSection/FifthSection";
+import SeventhSection from "../components/NewDesign/SeventhSection/SeventhSection";
+import DomainSection from "../components/NewDesign/DomainSection/DomainSection";
 import OfferPopup from "../components/OfferPopup/OfferPopup";
-import LearnSupport from "../components/LearnSupport/LearnSupport";
-import React, { useState } from "react";
+import { FullStackSoftwareCourseData } from "../Data/FullStackSoftware";
+import FAQNew from "../components/NewDesign/FAQNew/FAQNewcer";
 import {
-  FaqData8,
-  PaymentFaqData8,
-  CapstoneFaqData8,
-  CertificationFaqData8,
-  jobFaqData8,
-  MentorshipFaqData8,
-  SupportFaqData8,
+  FaqData9,
+  DomainFaqData9,
+  PaymentFaqData9,
+  CapstoneFaqData9,
+  jobFaqData9,
+  MentorshipFaqData9,
+  SupportFaqData9,
 } from "../components/FAQ/FaqData";
+import BatchDetails from "../components/NewDesign/BatchDetails/BatchDetails";
+import Project from "../components/NewDesign/Project/Project";
+import Switch from "../components/NewDesign/switch/switch";
+import ProgramCustomer from "../components/NewDesign/ProgramCustomer/ProgramCustomer";
+import GrowthStats from "../components/NewDesign/GrowthStat/GrowthStats";
+import Counselling from "../components/NewDesign/Counselling/Counselling";
 
 export default function Home() {
   const [popups, setPopups] = useState(false);
@@ -37,7 +38,7 @@ export default function Home() {
     setPopups(true);
   };
   return (
-    <div className={styles.container}>
+   <>
       <Head>
         <title>
           Data Structures Algorithms and System Design Course - DSA Course
@@ -61,8 +62,7 @@ export default function Home() {
         />
       </Head>
       <main>
-        {" "}
-        <Navbar popup={true} fullStack={true} />
+        <Navbar popup={true} dataScienceCounselling={true} />
         <Popup trigger={popups} setTrigger={setPopups} className="popupModal">
           <div className="leftPopup">
             <div className="whiteP" />
@@ -74,98 +74,186 @@ export default function Home() {
           </div>
         </Popup>
         <FirstSection
-          fsdBrochure={true}
-          fullStack={true}
-          deskTopPara="Data Structures Algorithms & System Design"
-          mTopPara="Data Structures Algorithms & System Design"
-          mTitle="Best Interview Prep Course for "
-          spanMTitleText="PRODUCT- Based Companies"
-          title="Best Interview Prep Course for "
-          spanTitleText="PRODUCT- Based Companies"
-          desc="Perform real-world industrial projects and use-cases."
-          src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/head/DSA.png"
-          width="776"
-          height="682"
-          alt="Full Stack"
-          srcD="https://brochureslearnbay.s3.ap-south-1.amazonaws.com/learnbay/Data+Structure+Algorithm+%26+System+Design+Learnbay.pdf"
+          softwareBtnHide={true}
+          FirstTyped="Microsoft & IBM Certificate"
+          SecondTyped="Domain Specialized Program"
+          ThirdTyped="Live Interactive Classes"
+          dataScience={true}
+          titleCourse="Data Structures Algorithms & System Design with Domain Specialization"
+          brochureLink="https://brochureslearnbay.s3.ap-south-1.amazonaws.com/learnbay/Full+Stack+Software+Development+Program+Learnbay.pdf"
+          first="Tools"
+          second="Tips"
+          third="Technology"
+          FirstRightImg="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/SW-Dev-Master-Header.png"
+          firstToparaImg="with Domain Specialization"
+          firstHeading="Data Structures Algorithms & System Design"
+          firstTopPara="Get ready to level up your coding expertise"
+          idss="bfl64ANfSV0"
+          srcD="https://brochureslearnbay.s3.ap-south-1.amazonaws.com/learnbay/Full+Stack+Software+Development+Program+Learnbay.pdf"
         />
-        <div className={styles.program}>
-          <ProgramInfo
-            p1="Premium Job"
-            p11="Placement Support"
-            p2="Job Advancement"
-            p22="Skills"
-            p3="Practice with"
-            p33="Real-Time Projects"
-            p4="Financing as low as"
-            p44="₹ 6,883/month"
-          />
-        </div>
-        <div className="Feature" id="Feature">
-          <BoxShape
-            title="Why Enroll In Data Structures Algorithms & System Design?"
-            Box1h5="Crack Interviews at MNC's"
-            box1desc="Learn in-depth DSA and crack interviews in product-based MNCs"
-            Box2h5="1:1 Mentorship"
-            box2desc="Guided by mentors working In MNCs to support you for the tech interviews"
-            Box3h5="Practical Project Experience"
-            box3desc="Gain technical expertise by working on challenging real-world projects"
-            Box4h5="250+ Hiring Partners"
-            box4desc="Get dedicated placement support with 100% job assistance"
-          />
-        </div>
-        <DomainFaq FaqData={DomainFaqCourseDataDSA} />
-        <CourseReview fsdBrochure={true} fullStack={true} />
-        <SyllabusNew
-          fsdBrochure={true}
-          fullStack={true}
-          syllabus={DsaCourseData[0].syllabus}
-          syllabusDesc={DsaCourseData[0].syllabusDesc}
-          popupHead={DsaCourseData[0].popupHead}
-          srcD="https://brochureslearnbay.s3.ap-south-1.amazonaws.com/learnbay/Data+Structure+Algorithm+%26+System+Design+Learnbay.pdf"
-          hours="200+ Hours"
+        <SecondSection
+          SecondHead="Why Choose Us?"
+          SecondPara="India's top rated  bootcamp for working professionals "
+          heading1="On-Demand Doubt Session"
+          heading2="IBM+ Microsoft Certificate"
+          heading3="Peer Learning & Networking"
+          heading4="Designed For Professionals"
+          SecondPara1="Clear your doubts with industry experts & get real-time solutions"
+          SecondPara2="Enhance skills with Microsoft & IBM course certificate"
+          SecondPara3="Learn & collaborate with peers for growth & opportunities"
+          SecondPara4="Boost your career growth with hands-on software training"
+        />
+        <ProgramCustomer
+          img1="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/1.png"
+          img2="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/1.png"
+          img3="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/1.png"
+          img4="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/1.png"
+          leftImage="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/SW+Dev+Masters+-+Who+is+this+program+for.png"
+          para1="Bachelor's degree with consistent good academics"
+          para2="Minimum 6 months of experience in IT/Non-IT domain"
+          para3="Computer Science, B.Tech, B.Sc. pursuing students"
+          para4="Designed for individuals seeking software development skills"
+          heading1="Education"
+          heading2="Work experience"
+          heading3="Career stage"
+          heading4="Aspirations"
+        />
+        <Switch />
+        <ThirdSection
+          secondLine="Invest in yourself!"
+          thirdHeadSpan=" Alumni Community"
+          thirdHead="Stay Ahead Of The Curve With Support Of Our"
+          point1="3+ years of career service access"
+          point2="6 mock interviews with industry leaders"
+          point3="Resume build up session"
+          point4="100% placement support"
+          BatchPara="The pie-chart representation here shows that the comprehensive Advanced Data Science and AI Foundation Program is suitable for people who want to take their tech and business reporting knowledge to an advanced level, coming from the following industries and designation:"
+          BatchParaImg1="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/Data+Science+and+AI+Advance+Left.png"
+          BatchParaImg2="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/Data+Science+and+AI+Advance+Right.png"
+          dataScience={true}
+          titleCourse="Full Stack Placement Report"
+          brochureLink="https://brochureslearnbay.s3.ap-south-1.amazonaws.com/learnbay/Placement+Report+(1).pdf"
+          img1="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/adobe.png"
+          img2="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/airbnb.png"
+          img3="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/antuit+ai.png"
+          img4="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/bloom+ai.png"
+          img5="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/cisco.png"
+          img6="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/datazymes.png"
+          img7="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/dell.png"
+          img8="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/flipkart.png"
+          img9="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/fractal+analytics.png"
+          img10="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/gartner.png"
+          img11="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/genpact.png"
+          img12="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/HCL.png"
+          img13="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/jp+morgan.png"
+          img14="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/KPMG.png"
+          img15="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/lenskart.png"
+          img16="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/linkedin.png"
+          img17="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/lti+mindtree.png"
+          img18="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/mercedes+benz.png"
+          img19="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/musigma.png"
+          img21="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/paypal.png"
+          img22="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/tiger+analytics.png"
+          img23="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/company-placement/tiger+analytics.png"
+        />
+        <Counselling dataScienceCounselling={true} />
+        <FourthSection
+          serviceBasic={true}
+          careerH1="Career Service"
+          careerSpan=" Basic"
+          dataScience={true}
+          software={true}
+          titleCourse="Data Structures Algorithms & System Design with Domain Specialization"
+          brochureLink="https://brochureslearnbay.s3.ap-south-1.amazonaws.com/learnbay/Full+Stack+Software+Development+Program+Learnbay.pdf"
+          syllabus={FullStackSoftwareCourseData[0].syllabus}
+          advSyllabus={FullStackSoftwareCourseData[0].advSyllabus}
+          syllabusDesc={FullStackSoftwareCourseData[0].syllabusDesc}
+          popupHead={FullStackSoftwareCourseData[0].popupHead}
+          srcD="https://brochureslearnbay.s3.ap-south-1.amazonaws.com/learnbay/Full+Stack+Software+Development+Program+Learnbay.pdf"
+          hours="250+ Hours"
           project="12+ Real Time"
         />
-        <ProgramFee
-          Fee="₹ 70,000 + 18% GST"
-          Emi="₹ 6,883/month"
-          fullStack={true}
+        <GrowthStats
+          img1="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/SW+Dev+Masters+Scope+Image-min.png"
+          heading="Embrace the future of technology & unlock endless career opportunities"
+          para1="Higher Growth: Compound annual growth rate of CAGR of 11.9% in 2023"
+          para2="Maximize Cloud Opportunities: Unleash your potential & earn 8 LPA"
+          para3="Strong Demand: Growing need for tech wizards across various industries"
         />
-        <div className={styles.ProjectWrapper} id="project">
-          <Project
-            project="12+ Projects"
-            domain="7+"
-            fsdBrochure={true}
-            fullStack={true}
-          />
-        </div>
-        <CourseFee
-          fullStack={true}
-          timeFsd={true}
-          syllabus={DsaCourseData[0].Batch}
-          CourseFeeHead="Data Structures Algorithms & System Design: Batch Details"
-          CourseFeePara="Online Classroom"
-          CourseFeelist1="Online Interactive Classes"
-          CourseFeelist2="Weekend and Weekday Batches"
-          CourseFeelist3="Extra Doubt Clearing Sessions"
-          CourseFeelist4="Multiple Domain Selection Options"
-          CourseFeelist5="Company-based Mock Interviews"
+
+        <FifthSection
+          softTools={true}
+          IBMMicrosoftsoft={true}
+          Fee="₹ 1,10,000 + 18% GST"
+          FeeEmi="₹ 10,817/month"
+          FeeHeading="Program Fee & Financing"
+          FeeContent1="0% interest rate"
+          FeeContent2="No cost EMI"
+          FeeContent3="Flexible payment"
+          FeeContent4="Easy loan procedure"
+          FeeContent5="15 days refund policy"
+          FeeContent6="No additional cost"
+          para="We are dedicated to making best course for Data Structures and Algorithms accessible. We are committed to helping you find a way to budget for this Course and offer a variety of financing options to make it more economical."
+          dataScience={true}
+          titleCourse="Data Structures Algorithms & System Design with Domain Specialization"
+          brochureLink="https://brochureslearnbay.s3.ap-south-1.amazonaws.com/learnbay/Full+Stack+Software+Development+Program+Learnbay.pdf"
+          img1="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/1.png"
+          img2="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/2.png"
+          img3="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/3.png"
+          img4="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/4.png"
+          img5="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/5.png"
+          img6="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/6.png"
+          img7="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/7.png"
+          img8="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/8.png"
+          img9="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/9.png"
+          img10="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/10.png"
+          img11="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/11.png"
+          img12="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/12.png"
+          img13="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/13.png"
+          img14="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/14.png"
+          img15="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/15.png"
+          img16="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/16.png"
+          img17="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/17.png"
+          img18="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/18.png"
+          img19="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/19.png"
+          img21="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/7.png"
+          img22="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/8.png"
+          img23="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/9.png"
+          img20="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/logos/24.jpeg"
         />
-        {/* chetna ne bola tha */}
-        {/* <FAQNew
-          FaqDatas={FaqData8}
-          PaymentFaqDatas={PaymentFaqData8}
-          CapstoneFaqDatas={CapstoneFaqData8}
-          CertificationFaqDatas={CertificationFaqData8}
-          jobFaqDatas={jobFaqData8}
-          MentorshipFaqDatas={MentorshipFaqData8}
-          SupportFaqDatas={SupportFaqData8}
-        /> */}
-        <LearnSupport />
+        <DomainSection
+          Software={true}
+          dataScience={true}
+          titleCourse="Data Structures Algorithms & System Design with Domain Specialization"
+          brochureLink="https://brochureslearnbay.s3.ap-south-1.amazonaws.com/learnbay/Full+Stack+Software+Development+Program+Learnbay.pdf"
+        />
+        <Project
+          SoftwareText={true}
+          project="12+ Projects"
+          tools="16+"
+          Software={true}
+          dataScience={true}
+          titleCourse="Full Stack Project Brochure"
+          brochureLink="https://brochureslearnbay.s3.ap-south-1.amazonaws.com/learnbay/Real-time+Projects+%26+Use+cases.pdf"
+        />
+        <BatchDetails
+          FullStack={true}
+          CourseFeeHead="Data Science and AI Foundation Program : Batch Details"
+        />
+        <FAQNew
+          FaqDatas={FaqData9}
+          DomainFaqDatas={DomainFaqData9}
+          PaymentFaqDatas={PaymentFaqData9}
+          CapstoneFaqDatas={CapstoneFaqData9}
+          jobFaqDatas={jobFaqData9}
+          MentorshipFaqDatas={MentorshipFaqData9}
+          SupportFaqDatas={SupportFaqData9}
+        />
+        <SeventhSection fullStackCont={true} />
         <OfferPopup offer={true} />
         <BottomBar />
         <Footer />
       </main>
-    </div>
+      </>
   );
 }
