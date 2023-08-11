d8c03e3184e9dc305ef27ddbc02f0dc270d04ecd
ammar-tanwar <ammartanwar123@gmail.com>
2023-08-11 18:04:34 +0530

    new refund content

--- components/refund/refund.js
+++ components/refund/refund.js
@@ -10,103 +10,79 @@ function refund() {
         </h4>
       </div>
       <div className={styles.refundInner}>
-        <p className={styles.top}>Refund-Cancellation Policy </p>
+        <p className={styles.top}>REFUND POLICY AND TERMS OF SERVICE</p>
 
         <p className={styles.bot}>
-          It would help if you had an internet connection and a current and
-          valid payment method to pay for any service, goods, or program made
-          available by Learnbay. We allow you to make payments with our
-          collaboration with payment gateways, and we are not allowed to retain
-          any of your information required during the transaction process.
+          <b>Refund Eligibility: </b>
+          Post a period of fifteen (15) calendar days from the date of
+          enrollment/payment, the user shall be deemed ineligible to make any
+          claims for refund under any circumstance or grounds. This stipulation
+          stands irrespective of the mode of payment availed by the user, be it
+          credit card EMI, partnership loans, direct bank transfers, Unified
+          Payments Interface (UPI), or any other means. The aforementioned
+          fifteen-day period shall be calculated commencing from the date on
+          which payment was effected.
         </p>
         <p className={styles.bot}>
-          Before utilizing their services, you must agree to the terms put forth
-          by any third-party company, such as PayPal, PAYTM, RazorPay, etc.
-          Understand that Learnbay is not liable for any information loss that a
-          third-party provider of payment services could cause. Must agree to
-          pay for services using whichever payment option is available on your
-          chosen Platform.
-        </p>
-
-        <p className={styles.top}>What does your approval mean to us?</p>
-
-        <p className={styles.bot}>
-          You agree to our Refund Policy by using our website, creating an
-          account, or making a purchase.
-        </p>
-
-        <p className={styles.top}>What's the refund cancellation policy?</p>
-
-        <p className={styles.bot}>
-          There are a few scenarios under which the refund policy will become
-          void:-
-        </p>
-
-        <p className={styles.bot}>
-          1. A user isn't eligible for a refund after the initial trial period
-          of 15 days unless they reached out to Learnbay before
+          <b>Extension Due to Emergencies: </b>
+          In events of personal exigencies precluding students from attending
+          live sessions, a formal request may be tabled seeking an intermission
+          not exceeding three (3) months. Subsequently, Learnbay shall extend
+          the live class subscription for an equivalent tenure. Notwithstanding
+          the above, if a student has opted for EMI payment mode, he/she remains
+          contractually bound to honour EMI commitments to our partnered loan
+          entities or credit card institutions.
         </p>
         <p className={styles.bot}>
-          <b>Note: </b>The Master Program is excluded from this please refer to
-          the Terms & Conditions specified for the Master Program.
+          <b>Breach of Terms: </b>
+          Any transgression or infringement of the stipulated Terms and
+          Conditions set forth on the Learnbay platform, followed by course
+          abandonment, shall result in Learnbay unilaterally revoking the user's
+          access.
         </p>
         <p className={styles.bot}>
-          2. In case, the user opted for a student loan via Learnbay, and the
-          same is already approved (when applying for a refund) approved
+          <b>Unauthorized Sharing: </b>
+          Dissemination or sharing of user credentials without the explicit
+          authorization of Learnbay may attract incremental fines and/or result
+          in immediate expulsion from the course.
         </p>
         <p className={styles.bot}>
-          <b>Note: </b>No refund or loan cancellation will be possible. The 15
-          days refund policy will get void here.
+          <b>Termination on Grounds of Misrepresentation: </b>
+          Should Learnbay ascertain the provision of misleading, spurious, or
+          unlawfully procured documentation, the organization reserves the
+          absolute right to indefinitely terminate the user's course access.
         </p>
         <p className={styles.bot}>
-          3. If a user violates any Terms and Conditions listed on the Learnbay
-          website and leaves the course, Learnbay will cease his/her access
+          <b>Document Submission: </b>
+          Should an enrollee fail to furnish mandatory documentation requisite
+          for course application post three (3) consecutive intimations,
+          Learnbay holds the right to discontinue the student's access to the
+          Learning Management System (LMS).
         </p>
         <p className={styles.bot}>
-          4. If a user shares credentials without the approval of Learnbay will
-          lead to an increase in fines or even expulsion from the course
+          <b>Refund Procedure: </b>
+          Upon sanctioning a refund, the amount shall be credited to the
+          original source employed during the transaction. For elucidation,
+          should a transaction be undertaken via credit card, the refund shall
+          be channeled to the same, typically within a window of five (5) to
+          seven (7) working days. It's imperative for the claimant to produce
+          verifiable proof of transaction or a valid receipt to initiate the
+          refund process.
         </p>
         <p className={styles.bot}>
-          5. If Learnbay has to cease (permanently) course access due to false
-          or inaccurate information/ illegally managed documents
+          <b>In the event of complications encountered during the refund process: </b>
+          Engage with the pertinent credit card institution, acknowledging potential delays in the reflection of the refund.
         </p>
         <p className={styles.bot}>
-          6. If candidates have not submitted the required documents for
-          application completion even after 3 successive notices, Learnbay has
-          to cease her/his LMS access
+        Subsequently, liaise with your banking entity, accounting for possible processing delays.
         </p>
         <p className={styles.bot}>
-          7. Enrolled in a course but have not attended the 70% of these trial
-          phase classes within the 15 days of enrolment
-        </p>
-
-        <p className={styles.top}>In case of refunds:-</p>
-
-        <p className={styles.bot}>
-          Refunds will be granted to the original source used at the time of
-          purchase. For example, if a payment is made through a credit card,
-          then the refund will be initiated in the same payment source, within
-          5-7 working days. **We require proof of purchase or receipt to
-          complete your return.
+        Should the refund remain unprocessed despite adherence to the aforementioned protocols, direct your communication to us at
+          <a href="mailto:contacts@learnbay.co"> <b>contacts@learnbay.co</b></a>
         </p>
         <p className={styles.bot}>
-          In case you get any difficulty, in the refund process then
-        </p>
-        <li className={styles.bot}>
-          Get in touch with your credit card provider because it can take some
-          time for the refund to appear on your account.
-        </li>
-        <li className={styles.bot}>
-          Next, contact your bank. There is often a delay in processing refunds.
-        </li>
-        <p className={styles.top}>
-          Having followed all of the above steps but still have not received
-          your refund, contact us at -{" "}
-          <a href="mailto:contacts@learnbay.co">contacts@learnbay.co</a>
-        </p>
-        <p className={styles.top}>
-          For Cancellations please email us at:{" "}
-          <a href="mailto:contacts@learnbay.co">contacts@learnbay.co.</a>
+          <b>Cancellation Protocol: </b>All solicitations for course cancellations are to be electronically communicated to
+          <a href="mailto:contacts@learnbay.co"> <b>contacts@learnbay.co.</b></a>
         </p>
       </div>
     </section>
