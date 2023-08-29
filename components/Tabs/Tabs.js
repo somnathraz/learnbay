50385e0ecf43b4f521034b39ea06e9fe817e2361
Mohd-w123 <codewithwazid@gmail.com>
2023-08-29 11:17:42 +0530

    fresher page complete

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -437,7 +437,7 @@ const Tabs = ({ handleIcon }) => {
                           <br />Program For Freshers
                         </h5>
                         <span>
-                          300+ Hours <BsDot /> 8 & 10 Months{" "}
+                           9 Months{" "}
                         </span>
                       </div>
                     </div>
@@ -507,7 +507,7 @@ const Tabs = ({ handleIcon }) => {
                       <div className={styles.ProLeft}>
                         <h5>Software Development Master Program</h5>
                         <span>
-                          400+ Hours <BsDot /> 9 & 11 Months{" "}
+                           11 Months{" "}
                         </span>
                       </div>
                     </div>
@@ -600,7 +600,7 @@ const Tabs = ({ handleIcon }) => {
                       <div className={styles.ProLeft}>
                         <h5>Software Development Master Program</h5>
                         <span>
-                          400+ Hours <BsDot /> 9 & 11 Months{" "}
+                          11 Months{" "}
                         </span>
                       </div>
                     </div>
@@ -737,7 +737,7 @@ const Tabs = ({ handleIcon }) => {
                           <br />Program For Freshers
                         </h5>
                         <span>
-                          300+ Hours <BsDot /> 8 & 10 Months{" "}
+                           9 Months{" "}
                         </span>
                       </div>
                     </div>
@@ -792,7 +792,7 @@ const Tabs = ({ handleIcon }) => {
                       <div className={styles.ProLeft}>
                         <h5>Software Development Master Program</h5>
                         <span>
-                          400+ Hours <BsDot /> 9 & 11 Months{" "}
+                           11 Months{" "}
                         </span>
                       </div>
                     </div>
@@ -1036,7 +1036,7 @@ const Tabs = ({ handleIcon }) => {
                       <div className={styles.ProLeft}>
                         <h5>Software Development Master Program</h5>
                         <span>
-                          400+ Hours <BsDot /> 9 & 11 Months{" "}
+                          11 Months{" "}
                         </span>
                       </div>
                     </div>
@@ -1071,7 +1071,7 @@ const Tabs = ({ handleIcon }) => {
                           <br />Program For Freshers
                         </h5>
                         <span>
-                          300+ Hours <BsDot /> 8 & 10 Months{" "}
+                          9 Months{" "}
                         </span>
                       </div>
                     </div>
@@ -1107,7 +1107,7 @@ const Tabs = ({ handleIcon }) => {
                       <div className={styles.ProLeft}>
                         <h5>Software Development Master Program</h5>
                         <span>
-                          400+ Hours <BsDot /> 9 & 11 Months{" "}
+                           11 Months{" "}
                         </span>
                       </div>
                     </div>
@@ -1209,7 +1209,7 @@ const Tabs = ({ handleIcon }) => {
                       <div className={styles.ProLeft}>
                         <h5>Software Development Master Program</h5>
                         <span>
-                          400+ Hours <BsDot /> 9 & 11 Months{" "}
+                           11 Months{" "}
                         </span>
                       </div>
                     </div>
@@ -1293,7 +1293,7 @@ const Tabs = ({ handleIcon }) => {
                           <br />Program For Freshers
                         </h5>
                         <span>
-                          300+ Hours <BsDot /> 8 & 10 Months{" "}
+                          9 Months{" "}
                         </span>
                       </div>
                     </div>
