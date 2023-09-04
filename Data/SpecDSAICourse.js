6b70f946ba3eb6cc2f217dbc2f417d6380a4942c
Mohd-w123 <codewithwazid@gmail.com>
2023-09-04 16:47:02 +0530

    DS batch details updated

--- Data/SpecDSAICourse.js
+++ Data/SpecDSAICourse.js
@@ -703,15 +703,15 @@ export const SpecDSAICourse = [
       ProMaxDesc: " Get data science training with 100% job assistance",
     },
 
-    Batch: [
+     Batch: [
       {
         Module0: {
-          title: "Weekday Morning Batch :- (Mon-Fri)",
+          title: "Weekday Evening Batch :- (Mon-Fri)",
           content: [
             {
               chap: {
                 title: "Batch 1 :- Date And time ",
-                desc: ["Date : 25th August 2023", "Time : 8 AM to 10 AM"],
+                desc: ["Date : 8th September 2023", "Time : 8 PM to 10 PM"],
               },
             },
           ],
@@ -721,12 +721,12 @@ export const SpecDSAICourse = [
       },
       {
         Module0: {
-          title: "Weekend Morning Batch :- (Sat-Sun)",
+          title: "Weekday Morning Batch :- (Mon-Fri)",
           content: [
             {
               chap: {
                 title: "Batch 1 :- Date And time ",
-                desc: ["Date : 27th August 2023", "Time : 9:30 AM to 1 PM"],
+                desc: ["Date : 22nd September 2023", "Time : 8 AM to 10 AM"],
               },
             },
           ],
